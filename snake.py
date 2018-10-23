5626bea93262410942b215cf95e05fb9c5ea931e
Samuel Rosander <s.rosander@gmail.com>
2018-10-23 22:53:41 +0200

    Working game  - Collision on self  - Restart option  - Better key listeners  - Food spawn on occupied square check

--- snake.py
+++ snake.py
@@ -20,89 +20,113 @@ class Snake(object):
 		self.vx = vx
 		self.vy = vy
 
-	def checkwalls(self,w,h):
+	def checkcrash(self,w,h,tail):
 		""" returns true if next move puts head outside window """
-		return self.x + self.vx >= w / 20 or self.x + self.vx < 0 or self.y + self.vy >= h / 20 or self.y + self.vy < 0
+		if self.x + self.vx >= w / 20 or self.x + self.vx < 0 or self.y + self.vy >= h / 20 or self.y + self.vy < 0:
+			return True
+		for i,t in enumerate(tail):
+			if self.x + self.vx == t["x"] and self.y + self.vy == t["y"] and i > 0:
+				return True
+		return False
 			
 WHITE = (255,255,255)
 BLACK = (0,0,0)
 RED = (255,0,0)
 
-framecount = 0
-(width,height) = (400,400)
-(foodx, foody) = (50,50)
+(width, height) = (400, 400)
+(foodx, foody) = (0, 0)
 
 pygame.init()
 win = pygame.display.set_mode((width,height))
 pygame.display.set_caption("Snake")
-snake = Snake(2,int(height/20//2))
+
+snake = Snake(2,9)
 tail = []
 
 pygame.display.flip()
 
 def main():
-	global framecount
+	global snake, tail
 	running = True
 	game_over = False
+	keys_locked = False # to prevent going backwards by tapping two keys fast in sequence
 	spawn_food()
 	while running:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				running = False
-		
-		keys = pygame.key.get_pressed()
-		if keys[pygame.K_LEFT] and snake.vx != 1:
-			snake.changedir(-1,0)
-		if keys[pygame.K_RIGHT] and snake.vx != -1:
-			snake.changedir(1,0)
-		if keys[pygame.K_UP] and snake.vy != 1:
-			snake.changedir(0,-1)
-		if keys[pygame.K_DOWN] and snake.vy != -1:
-			snake.changedir(0,1)
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_SPACE and game_over == True:
+					game_over = False
+					restart_game()
+				if event.key == pygame.K_ESCAPE:
+					running = False
+				if event.key == pygame.K_LEFT and snake.vx != 1 and not keys_locked:
+					keys_locked = True
+					snake.changedir(-1,0)
+				if event.key == pygame.K_RIGHT and snake.vx != -1 and not keys_locked:
+					keys_locked = True
+					snake.changedir(1,0)
+				if event.key == pygame.K_UP and snake.vy != 1 and not keys_locked:
+					keys_locked = True
+					snake.changedir(0,-1)
+				if event.key == pygame.K_DOWN and snake.vy != -1 and not keys_locked:
+					keys_locked = True
+					snake.changedir(0,1)
 
-		if framecount % 10 == 0 and not game_over:
-			if snake.checkwalls(width, height):
+		if not game_over:
+			keys_locked = False # activates movement key presses again
+
+			if snake.checkcrash(width, height, tail): # if crash, draw a game over frame
 				game_over = True
 				win.fill(WHITE)
 				snake.draw(RED)
-				for tailpart in tail:
-					pygame.draw.rect(win, BLACK, [tailpart["x"]*20, tailpart["y"]*20, 20, 20])
+				for t in tail:
+					pygame.draw.rect(win, BLACK, [t["x"]*20, t["y"]*20, 20, 20])
+				pygame.draw.circle(win, BLACK, (foodx*20+10,foody*20+10), 10)
 				pygame.display.update()
-			else:
-				if snake.x == foodx and snake.y == foody:
+			else: # draw a normal frame
+				if snake.x == foodx and snake.y == foody: # spawn new food when it gets eaten and add current square to tail
 					spawn_food()
-					grow(snake.x, snake.y)
-
-				for i, tailpart in enumerate(tail):
-					if i < len(tail)-1:
-						tail[i] = tail[i+1]
-					else:
-						tail[i] = {"x" : snake.x, "y" : snake.y}
+					tail.append({"x" : snake.x, "y" : snake.y})
+				else: # shift the tail pieces on position in the list
+					for i, tailpart in enumerate(tail):
+						if i < len(tail)-1:
+							tail[i] = tail[i+1]
+						else:
+							tail[i] = {"x" : snake.x, "y" : snake.y}
 
 				win.fill(WHITE)
 				snake.move()
 				snake.draw(BLACK)
 
-				for tailpart in tail:
-					pygame.draw.rect(win, BLACK, [tailpart["x"]*20, tailpart["y"]*20, 20, 20])
+				for tailpart in tail: #draw the tail
+					pygame.draw.rect(win, BLACK, [tailpart["x"]*20, tailpart["y"]*20, 20, 20]) 
 
-				pygame.draw.circle(win, BLACK, (foodx*20+10,foody*20+10), 10)
+				pygame.draw.circle(win, BLACK, (foodx*20+10,foody*20+10), 10) # draw the food
 				pygame.display.update()
 
-		framecount += 1
-		#print(tail)
-		pygame.time.delay(10)
+		pygame.time.delay(100)
 
 	pygame.quit()
+
+def restart_game():
+	global snake, tail
+
+	spawn_food()
+	snake = Snake(2,9)
+	tail = []
 
 def spawn_food():
 	global foodx, foody
 
-	foodx = floor(randint(0,width/20-1))
-	foody = floor(randint(0,height/20-1))
+	while 1: # generate a new x and y value if it's already occupied by the tail
+		randx = floor(randint(0,width/20-1))
+		randy = floor(randint(0,height/20-1))
 
-def grow(x, y):
-	global tail
-	tail.append({"x" : x, "y" : y})
-		
+		if {"x" : randx, "y" : randy} not in tail:
+			foodx = randx
+			foody = randy
+			break
+
 main()
